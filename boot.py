7f17a090c2bc8f89c062f14800913e1db7f6975d
nanyij-world <59493899+nanyij-world@users.noreply.github.com>
2020-01-08 23:59:01 +0800

    Add files via upload

---
+++ boot.py
@@ -0,0 +1,84 @@
+#
+# example ESP8266 or ESP32 Huzzah mqtt publish/subscribe with io.adafruit.com
+# phil van allen
+#
+# thanks to https://github.com/MikeTeachman/micropython-adafruit-mqtt-esp8266/blob/master/mqtt-to-adafruit.py
+#
+
+import network
+import time
+import machine
+from servo import Servo
+from umqtt.simple import MQTTClient
+
+pin = machine.Pin(13, machine.Pin.OUT) # LED on the board
+button = machine.Pin (34, machine.Pin.IN)
+led = machine.Pin(26, machine.Pin.OUT) # LED on the board
+servo_pin1 = machine.Pin(4)
+my_servo1 = Servo(servo_pin1)
+my_servo1.write_angle(0)
+headcount = 0
+
+def sub_cb(topic, msg):
+    value = float(str(msg,'utf-8'))
+    print("subscribed value = {}".format(value))
+    if value > 4:
+        pin.value(1)
+    else:
+        pin.value(0)
+
+#
+# connect the ESP to local wifi network
+#
+yourWifiSSID = "ACCD"
+yourWifiPassword = "tink1930"
+sta_if = network.WLAN(network.STA_IF)
+if not sta_if.isconnected():
+    sta_if.active(True)
+    sta_if.connect(yourWifiSSID, yourWifiPassword)
+    while not sta_if.isconnected():
+        pass
+print("connected to WiFi")
+
+#
+# connect ESP to Adafruit IO using MQTT
+#
+myMqttClient = "heyheyclient"  # replace with your own client name
+adafruitUsername = "njiang"  # can be found at "My Account" at adafruit.com
+adafruitAioKey = "34446144134c45b18575c8527982b616"  # can be found by clicking on "VIEW AIO KEYS" when viewing an Adafruit IO Feed
+adafruitFeed = adafruitUsername + "/feeds/mydata" # replace "test" with your feed name
+adafruitIoUrl = "io.adafruit.com"
+
+c = MQTTClient(myMqttClient, adafruitIoUrl, 0, adafruitUsername, adafruitAioKey)
+c.set_callback(sub_cb)
+c.connect()
+c.subscribe(bytes(adafruitFeed,'utf-8'))
+
+
+while True:
+#    print(button.value())
+    if button.value() == 1:
+        led.value(1)
+        time.sleep(10)
+        headcount = headcount + 1
+        print ("Current headcount: ", headcount)
+        c.publish(adafruitFeed,str(headcount))
+        c.check_msg()
+        for i in range(0,180,1):
+          if i == 60 or i == 120 or i == 180 :
+              time.sleep(5)
+          else:
+              my_servo1.write_angle(i)
+              time.sleep(0.16)
+        print("servo turning!")
+        for i in range(180,-1,-1):
+              my_servo1.write_angle(i)
+              time.sleep(0.025)
+        print("servo backing up!")
+
+    else:
+        led.value(0)
+    time.sleep(1.0)
+
+c.disconnect()
+
